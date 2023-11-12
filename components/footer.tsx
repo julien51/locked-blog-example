fb4c98f21eaccf0888fd82d33545e2b29cac5963
Julien Genestoux <julien.genestoux@gmail.com>
2023-11-12 23:51:07 +0300

    link fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           A demo of a token-gated blog built with Next.js and{' '}Unlock Protocol!
-          More <Link src="https://github.com/julien51/locked-blog-example/">details on Github!</Link>
+          More <Link href="https://github.com/julien51/locked-blog-example/">details on Github!</Link>
         </div>
       </Container>
     </footer>
